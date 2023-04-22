46cdd6a7cc732d9ea2ac9d291554317fd2bb8ea8
polarisjyb <polarislsj@gmail.com>
2023-04-23 02:11:47 +0900

    python - 사칙연산 몫_구하기

---
+++ Basic/python/사칙연산/몫_구하기.py
@@ -0,0 +1,10 @@
+def solution(num1, num2):
+    answer = num1 // num2
+    return answer
+
+print( solution(2, 6) )
+print( solution(6, 2) )
+print( solution(2, 2) )
+print( solution(3, -1) )
+print( solution(8, 100) )
+print( solution(5, 700) )
