894d81b90db41fc88f7bdc19cbee6060db1b127e
polarisjyb <polarislsj@gmail.com>
2023-04-25 00:31:02 +0900

    javascript - 수학 짝수의_합

---
+++ Basic/javascript/수학/짝수의_합.js
@@ -0,0 +1,26 @@
+// 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+
+// 제한사항
+// 0 < n ≤ 1000
+
+// 입출력 예 설명
+
+// 입출력 예 #1
+// n이 10이므로 2 + 4 + 6 + 8 + 10 = 30을 return 합니다.
+
+// 입출력 예 #2
+// n이 4이므로 2 + 4 = 6을 return 합니다.
+
+
+
+const solution = (n) => {
+  let sum = 0;
+  for (let i=1; i <= n; i++) {
+    if ( i%2 === 0) sum += i;
+  };
+  return sum;
+};
+
+solution(100);
+
+console.log(solution(100));
