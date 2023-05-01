f77f00308f86447688717c3ee5cd1862ead663fe
polarisjyb <polarislsj@gmail.com>
2023-05-02 00:41:50 +0900

    피자_나눠_먹기_2 정답 코드 추가

--- Basic/javascript/수학/피자_나눠_먹기_2.js
+++ Basic/javascript/수학/피자_나눠_먹기_2.js
@@ -20,8 +20,12 @@
 const solution = n => {
   let answer = 1;
 
-  while ( answer * 6 % n ) {
-      answer += 1;
+  // while ( answer * 6 % n ) {
+  //     answer += 1;
+  // };
+
+  while ( answer * 6 % n !== 0 ) {
+    answer += 1;
   };
   
   return answer;
