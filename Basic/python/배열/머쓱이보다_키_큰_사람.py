f5f86c34ca0682d95779c482259d8ef030dbdc72
polarisjyb <polarislsj@gmail.com>
2023-05-02 23:27:24 +0900

    Python - 머쓱이보다_키_큰_사람 풀이 추가

--- Basic/python/배열/머쓱이보다_키_큰_사람.py
+++ Basic/python/배열/머쓱이보다_키_큰_사람.py
@@ -18,8 +18,13 @@
 def solution(array, height):
     answer = 0
     
-    for i in range(len(array)):
-        if array[i] > height:
+    # for i in range(len(array)):
+    #     if array[i] > height:
+    #         answer += 1
+            
+    # array 요소에 접근
+    for i in array:
+        if i > height:
             answer += 1
     
     return answer
