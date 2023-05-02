42245ee4e0c7ca782933a12680f4847665ca1788
polarisjyb <polarislsj@gmail.com>
2023-05-03 00:58:52 +0900

    javascript - 배열 배열의_유사도

---
+++ Basic/javascript/배열/배열의_유사도.js
@@ -0,0 +1,31 @@
+// 두 배열이 얼마나 유사한지 확인해보려고 합니다.
+// 문자열 배열 s1과 s2가 주어질 때 같은 원소의 개수를 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ s1, s2의 길이 ≤ 100
+// 1 ≤ s1, s2의 원소의 길이 ≤ 10
+// s1과 s2의 원소는 알파벳 소문자로만 이루어져 있습니다
+// s1과 s2는 각각 중복된 원소를 갖지 않습니다.
+
+// 입출력 예 설명
+
+// 입출력 예 1
+// "b"와 "c"가 같으므로 2를 return합니다.
+
+// 입출력 예 2
+// 같은 원소가 없으므로 0을 return합니다.
+
+const solution = (s1, s2) => {
+  let answer = 0;
+  
+  for (let i in s1) {
+    for (let j in s2) {
+      if (s1[i] === s2[j]) answer++;
+    };
+  };
+  
+  return answer;
+};
+
+console.log( solution(["a", "b", "c"], ["com", "b", "d", "p", "c"]) );
+console.log( solution(["n", "omg"], ["m", "dot"]) );
